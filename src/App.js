fd6b257ac7326bb9f09bfe75304ac7c87719c121
RoyalDemonsNFT <96981437+RoyalDemonsNFT@users.noreply.github.com>
2022-01-03 00:20:58 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -255,16 +255,6 @@ function App() {
                   margin: "5px",
                 }}
               >
-                Roadmap
-              </StyledButton>
-              <StyledButton
-                style={{
-                  margin: "5px",
-                }}
-                onClick={(e) => {
-                  window.open(CONFIG.MARKETPLACE_LINK, "_blank");
-                }}
-              >
                 {CONFIG.MARKETPLACE}
               </StyledButton>
             </span>
